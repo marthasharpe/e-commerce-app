bf159bca7d28be6166507f3331b0430ff72702a3
Martha Sharpe <marthasharpe2020@gmail.com>
2023-10-31 14:38:06 -0500

    "create dialog and modal"

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,12 +1,18 @@
+"use client";
 import { Button } from "@/components/ui/button";
+import { Modal } from "@/components/ui/modal";
 
 export default function Home() {
   return (
-    <main className="m-4">
-      <div>
-        <p>Dashboard</p>
-        <Button>Button</Button>
-      </div>
-    </main>
+    <div>
+      <Modal
+        title="test"
+        description="test description"
+        isOpen
+        onClose={() => {}}
+      >
+        children
+      </Modal>
+    </div>
   );
 }
